78838ca8375dbd344d2719291c121d7e0b9b9508
Ken Corley <kenc138@gmail.com>
2020-08-01 18:47:00 -0600

    Clean up spacing

--- spec/strings/strings_spec.rb
+++ spec/strings/strings_spec.rb
@@ -2,24 +2,24 @@ require_relative 'strings'
 
 RSpec.describe BasicString do
 
-	before do
-		@test_word = "Ruby"
-  	@sentence = "There is a gemstone called ruby in existence"
-  	@text = BasicString.new(@sentence)
-	end
-	
+  before do
+    @test_word = "Ruby"
+    @sentence = "There is a gemstone called ruby in existence"
+    @text = BasicString.new(@sentence)
+  end
+  
   context "case-sensitive" do
-  	it "should output interpolated text" do
-  	  result = @text.contains_word?(@test_word)
-  	  expect(result).to be_falsey
-  	end
+    it "should output interpolated text" do
+      result = @text.contains_word?(@test_word)
+      expect(result).to be_falsey
+    end
   end
 
   context "case-insensitive" do
-  	it "should output interpolated text" do
-  	  result = @text.contains_word_ignorecase?(@test_word)
-  	  expect(result).to be_truthy
-  	end
+    it "should output interpolated text" do
+      result = @text.contains_word_ignorecase?(@test_word)
+      expect(result).to be_truthy
+    end
   end
 
 end
